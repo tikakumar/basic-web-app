03eac12580393a4009a078fbc0ecbb32a7724d00
Krutika Kumar <tikakumar@krutikas-mbp.wifi.local.cmu.edu>
2023-09-19 15:53:45 -0400

    Added support for the query 'What is x plus y?'

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,6 +16,12 @@ export default function QueryProcessor(query: string): string {
       "tika5"
     );
   }
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
 
   return "";
 }
